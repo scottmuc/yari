63084c64e22676f58c7e247d3891b688953ec3e3
Scott Muc <scottmuc@gmail.com>
2011-11-13 13:37:51 -0800

    Made batch script simpler and easier to follow
    Added the ability to add ruby versions

--- rubyenv.bat
+++ rubyenv.bat
@@ -1,27 +1,21 @@
 @echo off
+if "%*" == "" goto :usage
 
-if "%1" == "/?" goto :usage
+@powershell -ExecutionPolicy RemoteSigned .\rubyinstaller.ps1 %*
 
-for /f "delims=" %%I in ('powershell -file rubyinstaller.ps1') do set PATH=%%I;%PATH%
+if not exist yari.tmp.cmd (
+    echo something didn't work quite right...
+    goto :usage
+)
 
-if "%1" == "user" goto :user
-if "%1" == "machine" goto :machine
-goto :end
-
-:user
-for /f "delims=" %%I in ('powershell -file rubyinstaller.ps1') do setx PATH "%%I"
-goto :end
-
-:machine
-for /f "delims=" %%I in ('powershell -file rubyinstaller.ps1') do setx PATH "%%I;%PATH%" -m
+call yari.tmp.cmd
+del yari.tmp.cmd
 goto :end
 
 :usage
 echo This script will setup your ruby environment
 echo. 
-echo rubyenv.bat                sets ruby for the session
-echo rubyenv.bat user           sets ruby for the user
-echo rubyenv.bat machine        sets ruby for the machine
+echo rubyenv.bat version               sets ruby for the session
 echo. 
 goto :end
 
--- rubyinstaller.ps1
+++ rubyinstaller.ps1
@@ -1,34 +1,70 @@
+[CmdletBinding()]
+param(
+    [Parameter(Position=0,Mandatory=$true)]
+    [string] $version,
+    [Parameter(Position=1,Mandatory=$false)]
+    [string] $scope = "session"
+)
+
+$ErrorActionPreference = "Stop"
+
+$registry = @{
+    "1.8.7" = @{
+        "url" = "http://rubyforge.org/frs/download.php/74296/ruby-1.8.7-p334-i386-mingw32.7z";
+        "gem_path" = "lib\ruby\gems\1.8\bin"
+    };
+    "1.9.2" = @{
+        "url" = "http://rubyforge.org/frs/download.php/75128/ruby-1.9.2-p290-i386-mingw32.7z";
+        "gem_path" = "lib\ruby\gems\1.9.1\bin"
+    };
+    "devkit" = @{
+        "url" = "http://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20110712-1620-sfx.exe"
+    }
+}
+
 function Download-File {
-    param($url, $localFile = (Join-Path $pwd.Path $url.SubString($url.LastIndexOf('/'))) )
+    param($url, $localFile = (Join-Path $pwd.Path $url.SubString($url.LastIndexOf('/') + 1)))
         $client = New-Object System.Net.WebClient
         $client.DownloadFile($url, $localFile)
 }
 
-function Install-Devkit($ruby_dir, $devkit_dir) {
+function Install-Devkit {
+    param($ruby_dir, $devkit_dir) 
     if (Test-Path "$ruby_dir\lib\ruby\site_ruby\devkit.rb") { return }
     $env:PATH = "$ruby_dir\bin"
-    Install-Devkit $ruby_dir $devkit_dir 
-    & ruby $devkit_dir\dk.rb init | Out-Null 
+    & ruby $devkit_dir\dk.rb init
     "- $ruby_dir".Replace("\", "/") | Out-File config.yml -NoClobber -Append -Encoding ASCII
-    & ruby $devkit_dir\dk.rb install | Out-Null
+    & ruby $devkit_dir\dk.rb install
     del config.yml
 }
 
 function Install-Ruby {
-    $ruby_url = "http://rubyforge.org/frs/download.php/75128/ruby-1.9.2-p290-i386-mingw32.7z"
-    $devkit_url = "http://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20110712-1620-sfx.exe"
+    param($version)
+    $ruby_url = $registry."$version".url 
+    $devkit_url = $registry.devkit.url 
     $ruby_filename = $ruby_url.SubString($ruby_url.LastIndexOf('/') + 1)
     $devkit_filename = $devkit_url.SubString($devkit_url.LastIndexOf('/') + 1)
-    $ruby_dir = "$pwd\ruby-1.9.2-p290-i386-mingw32"
-    $devkit_dir = "$pwd\devkit"
 
     if (-not (Test-Path .\$ruby_filename)) { Download-File $ruby_url }
     if (-not (Test-Path .\$devkit_filename)) { Download-File $devkit_url }
+
+    $ruby_archive = (Get-ChildItem $ruby_filename)
+    $ruby_dir = "$pwd\$($ruby_archive.BaseName)"
+    $devkit_dir = "$pwd\devkit"
+
     if (-not (Test-Path $ruby_dir)) { & .\7z.exe x -y $ruby_filename | Out-Null }
     if (-not (Test-Path $devkit_dir)) { & .\7z.exe x -y -odevkit $devkit_filename | Out-Null }
 
-    Install-Devkit $ruby_dir $devkit_dir
-    Write-Host "$ruby_dir\bin;$ruby_dir\lib\ruby\gems\1.9.1\bin"
+    $gem_bin_dir = "$ruby_dir\$($registry.`"$version`".gem_path)"
+    Install-Devkit $ruby_dir $devkit_dir | Out-Host
+    return "$ruby_dir\bin;$gem_bin_dir"
 }
 
-Install-Ruby
+function Create-TempEnvScript {
+    param( [string] $path_augmentation)
+    "set PATH=$path_augmentation;%PATH%
+ruby -v" | Out-File yari.tmp.cmd -Encoding ASCII
+}
+
+$path_aug = Install-Ruby $version
+Create-TempEnvScript $path_aug
